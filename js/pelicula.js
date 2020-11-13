9f9d1d385d3ac26c355c25511b56ef9650e9268e
Apple <apple@Joaco-cukier.lan>
2020-11-13 17:09:33 +0000

    l

--- js/pelicula.js
+++ js/pelicula.js
@@ -13,11 +13,11 @@ console.log(queryObject)
 //3 Obtener el dato para completar el endpoint//
 
 let id = queryObject.get('id');
-console.log(id)
+console.log(id) 
 
 let apiKey = "4aafc89b60967c61ce5438ca044af061"
 
-let url = "https://api.themoviedb.org/3/movie/${id}?api_key=apiKey&language=en-US"
+let url = `https://api.themoviedb.org/3/movie/${id}?api_key=apiKey&language=en-US`
 
 let destino = document.querySelector('.infopeli');
 
@@ -28,24 +28,21 @@ fetch(url)
     .then(function(data){
         console.log(data)
 
-    let info = data.results
+    let info = data
 
-    
- 
-   //Aca ponemos nuestro HTML de pelicula donde se va a poner la informacion// 
-   //destino.innerHTML += ''//
-    
+    //Aca ponemos nuestro HTML de pelicula donde se va a poner la informacion// 
+   
    destino.innerHTML +=  `<div class="infopeli">
-   <h2> ${info.title} </h2>
+                        <h2> ${info.title} </h2>
 
-   <p class="no-mobile">Una asesina embarazada, cuyo nombre en código es La novia, entra en coma durante cuatro años después de que su exjefe Bill la ataca brutalmente. Cuando se despierta, se propone vengarse de él y sus asociados.</p>
+                        <p class="no-mobile">Una asesina embarazada, cuyo nombre en código es La novia, entra en coma durante cuatro años después de que su exjefe Bill la ataca brutalmente. Cuando se despierta, se propone vengarse de él y sus asociados.</p>
 
-   <ul class="no-mobile" type= "circle">
-       <li class= "director" >Director: Quentin Tarantino</li>
-       <li>Género: Acción, Película de violación y venganza, Artes marciales y cine gore</li>
-       <li>Actores: Uma Thurman, David Carradine, Lucy Liu, Vivica A. Fox, Michael Madsen, Daryl Hannah, Gordo Liu, Julie Dreyfus</li>
-   </ul>
-   </div>`    
+                        <ul class="no-mobile" type= "circle">
+                            <li class= "director" >Director: Quentin Tarantino</li>
+                            <li>Género: Acción, Película de violación y venganza, Artes marciales y cine gore</li>
+                            <li>Actores: Uma Thurman, David Carradine, Lucy Liu, Vivica A. Fox, Michael Madsen, Daryl Hannah, Gordo Liu, Julie Dreyfus</li>
+                        </ul>
+                        </div>`    
 
      
             
@@ -55,4 +52,4 @@ fetch(url)
     })
     .catch( function(error){
         console.log(error);
-    })
+    }) 
