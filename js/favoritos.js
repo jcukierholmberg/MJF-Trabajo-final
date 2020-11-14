89a3fe8aeca2340bcd8d2466251a3ef8eea119a3
Apple <apple@Joaco-cukier.lan>
2020-11-14 22:16:51 +0000

    lpm

--- js/favoritos.js
+++ js/favoritos.js
@@ -6,7 +6,7 @@ let mensaje = prompt ("Tenes creada una cuenta?")
 //Cambiar para que no sea un texto sino que sea un coso de opciones//
 
 if (mensaje == "si" ) {
-    alert ("apreta ok para iniciar sesión");
+    alert ("apreta 'ok' para ir a iniciar sesión");
 
     //mandarlo a la pagina de Inicio de sesion//
 
@@ -14,7 +14,7 @@ if (mensaje == "si" ) {
 
 else if (mensaje == "no" )
 {
-    alert ("apreta ok para crearte una cuenta");
+    alert ("apreta 'ok' para crearte una cuenta");
 
     window.location.href = "formulario.html";
 }
