1b15ffcb25c7d3e1d3f5f5af11c7aa043cc8002f
Apple <apple@Joaco-cukier.lan>
2020-11-14 20:33:22 +0000

    pagina favoritos y acomodando

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-//bienvenidos a sitio//
-let mensaje = prompt("¿Tienes una cuenta?")
 
-if (mensaje == "si" ) {
-    alert ("Bienvenido al sitio 'nombre de la persona'")
-}
-else if (mensaje == "no") {
-    alert ("Create una cuenta gratuita");
-}
+// Lo pongo en verde asi no jode cada vez que abrimos la pagina//
+
+// //bienvenidos a sitio//
+// let mensaje = prompt("¿Tienes una cuenta?")
+
+// if (mensaje == "si" ) {
+//     alert ("Bienvenido al sitio 'nombre de la persona'")
+// }
+// else if (mensaje == "no") {
+//     alert ("Create una cuenta gratuita");
+// }
 
 //URL//
 let media = "movie"
